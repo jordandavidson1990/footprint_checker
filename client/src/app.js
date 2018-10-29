25f76cc64382a1f5d64d36fbc0998aa0eefb9e24
Asma <asma.virgo85@gmail.com>
2018-10-29 12:22:02 +0000

    result graph working with test data

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,16 @@
 const CarbonFootprints = require('./models/carbon_footprint_data.js');
-const FormWrapperView = require('./views/form_wrapper.js')
-const Questions = require('./models/questions.js')
+const FormWrapperView = require('./views/form_wrapper.js');
+const Questions = require('./models/questions.js');
+const DisplayResultsChart = require('./views/display_results_chart.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-console.log("working");
+  console.log("working");
+
+  const resultsChartContainer = document.querySelector('#resultsChart');
+  const displayResultsChart = new DisplayResultsChart(resultsChartContainer);
+  displayResultsChart.bindEvents();
+
   const formWrapper = document.querySelector('#wrapper');
   const formWrapperView = new FormWrapperView(formWrapper);
   formWrapperView.bindEvents();
---
+++ client/src/views/display_results_chart.js
@@ -0,0 +1,36 @@
+const PubSub = require('../helpers/pub_sub.js');
+const GoogleChartsModule = require('google-charts');
+
+const DisplayResultsChart = function(container){
+  this.container = container;
+};
+
+DisplayResultsChart.prototype.bindEvents = function () {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = "Test Graph Area";
+  this.container.appendChild(paragraph);
+
+  GoogleChartsModule.GoogleCharts.load(() => {
+      const data = GoogleChartsModule.GoogleCharts.api.visualization.arrayToDataTable(
+        [['Category', 'Your CO2 Footprints', 'UK Average Person Footprints'],
+        ['Air Travel', 100, 70],
+        ['Diet', 200, 300],
+        ['Transport', 500, 200],
+        ['Home', 300, 300]]
+      );
+
+      var options = {
+        title: 'CO2 Footprint Results',
+        // legend: { position: 'top', maxLines: 2 },
+        colors: ['#5C3292', '#1A8763', '#871B47', '#999999'],
+        interpolateNulls: false,
+      };
+
+      const resultChart = new GoogleChartsModule.GoogleCharts.api.visualization.ColumnChart(document.querySelector('#resultsChart'));
+      // areaChart.draw(data);
+      resultChart.draw(data, options);
+});
+
+}
+
+module.exports = DisplayResultsChart;
