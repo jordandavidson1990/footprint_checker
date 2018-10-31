28af83912dace6384323e0de6ce0ec7c0d11762b
Asma <asma.virgo85@gmail.com>
2018-10-31 11:44:08 +0000

    fixes display graph view

--- client/src/views/display_results_chart.js
+++ client/src/views/display_results_chart.js
@@ -6,27 +6,59 @@ const DisplayResultsChart = function(container){
 };
 
 DisplayResultsChart.prototype.bindEvents = function () {
-
-
   PubSub.subscribe("CarbonFootprints:chart-data-ready", (evt) => {
     GoogleChartsModule.GoogleCharts.load(() => {
-        const data = GoogleChartsModule.GoogleCharts.api.visualization.arrayToDataTable(evt.detail);
 
-        var options = {
-          title: 'CO2 Footprint Results',
-          // legend: { position: 'top', maxLines: 2 },
-          colors: ['#5C3292', '#226e0a', '#a3d7ed', '#999999'],
-          interpolateNulls: false,
-        };
-
-        const resultChart = new GoogleChartsModule.GoogleCharts.api.visualization.ColumnChart(document.querySelector('#resultsChart'));
-        // areaChart.draw(data);
-        resultChart.draw(data, options);
-  });
+        this.drawTableAndChart(evt.detail);
+    });
   })
-
-
-
 }
+
+DisplayResultsChart.prototype.drawTableAndChart = function (inputArray) {
+  let combinedDataArray = new google.visualization.DataTable();
+  combinedDataArray.addColumn('string', inputArray[0][0]);
+  combinedDataArray.addColumn('number', inputArray[0][1]);
+  combinedDataArray.addColumn('number', inputArray[0][2]);
+  combinedDataArray.addRows(inputArray.length - 1);
+
+  for (let i = 1; i < inputArray.length ; i++){
+    for (let j = 0; j < inputArray[0].length; j++ ){
+      combinedDataArray.setCell(i - 1, j, inputArray[i][j]);
+    }
+  }
+  debugger;
+
+  let formatter = new google.visualization.NumberFormat({suffix: 'kg'});
+  formatter.format(combinedDataArray, 1);
+  formatter.format(combinedDataArray, 2);
+
+  let view = new google.visualization.DataView(combinedDataArray);
+  view.setColumns([0, 1, 2]);
+
+
+  let table = new google.visualization.Table(document.querySelector('#resultsChart'));
+  table.draw(view, {width: '100%'});
+
+  // {width: '80%', height: '100%'}
+
+  let chartOptions = {
+    title: 'Annual CO2 Footprint Results',
+    // legend: { position: 'top', maxLines: 2 },
+    colors: ['#5C3292', '#1A8763'],
+    interpolateNulls: false,
+    hAxis: {
+              title: 'CO2 footprints in kgs',
+            }
+  };
+
+  let chart = new google.visualization.BarChart(document.querySelector('#resultsTable'));
+  chart.draw(view, chartOptions);
+
+  google.visualization.events.addListener(table, 'sort',
+    function(event) {
+      combinedDataArray.sort([{column: event.column, desc: !event.ascending}]);
+      chart.draw(view, chartOptions);
+    });
+};
 
 module.exports = DisplayResultsChart;
