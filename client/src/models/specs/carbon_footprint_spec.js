81ed83bdbaf555e4b268509af5d78af43d58bacc
Asma <asma.virgo85@gmail.com>
2018-10-31 10:19:02 +0000

    updates specs files

--- client/src/models/specs/carbon_footprint_spec.js
+++ client/src/models/specs/carbon_footprint_spec.js
@@ -13,4 +13,15 @@ describe('CarbonFootprints', function(){
     assert.strictEqual(actual, 'http://localhost:3000/api/carbonFootprints');
   });
 
+  // it('should calculate Air Travel Footprints', function(){
+  //   const userInputAirTravel = {
+  //                               "Q1": "1000",
+  //                               "Q2": "Business",
+  //                               "Q3": "short_haul"
+  //                               }
+  //   carbonFootprints1.getData();
+  //   actual = carbonFootprints1.calculateAirTravelFootprint(userInputAirTravel);
+  //   assert.strictEqual(actual, 126);
+  // });
+
 })
